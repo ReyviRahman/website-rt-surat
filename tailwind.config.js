9491fd7af2d5b5037845a203da242f80a8fcac64
Reyvi Rahman <reyvirahman@gmail.com>
2024-12-08 16:55:41 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF9900',
+        secondary: '#000000',
+        orange: '#FEB811'
+      },
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      }
+    },
+  },
+  plugins: [],
+}
+
